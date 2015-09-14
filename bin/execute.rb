2c19a9885b22e66ee1f43c7559d6cae75c17becb
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-13 23:01:03 -0500

    Add view of complete/incomplete todos

--- bin/execute.rb
+++ bin/execute.rb
@@ -5,15 +5,21 @@ require_relative '../lib/view'
 
 view = View.new
 
-view.start
-input = view.get_selection
-
-if input == 1
-  view.create_todo
-elsif input == 2
-  view.edit_todo
-elsif input == 3
-  view.complete_todo
-elsif input == 4
-  view.delete_todo
+loop do
+  system('clear')
+  view.start
+  view.show_todos
+  input = view.get_selection
+  case input
+    when 1
+      view.create_todo
+    when 2
+      view.edit_todo
+    when 3
+      view.complete_todo
+    when 4
+      view.delete_todo
+    else
+      exit
+  end
 end
--- lib/view.rb
+++ lib/view.rb
@@ -7,26 +7,51 @@ class View
     @todolist = []
   end
 
+  def show_todos
+    @todolistview = []
+    Todo.find_each do |score|
+      @todolistview.push(score)
+    end
+
+    if @todolistview.size > 0
+      puts "-----Unfinished-----"
+      @todolistview.each do |todo|
+        if todo[:complete] == false
+          puts "#{todo.id}) #{todo[:name]}"
+        end
+      end
+
+      puts "\n-----Completed-----" if @todolistview.any? { |todo| todo[:complete] == true }
+
+      @todolistview.each do |todo|
+        if todo[:complete] == true
+          puts "#{todo.id}) #{todo[:name]}"
+        end
+      end
+    end
+    puts ""
+  end
+
   def start
-    puts "\n"
     puts "-"*30
     puts "   Welcome to THE To-Do App."
     puts "-"*30
     puts "\n"
-    puts "Please make a selection: "
   end
 
   def get_selection
+    puts "Please make a selection:"
     puts "1) Create To-Do"
     puts "2) Edit To-Do"
     puts "3) Complete To-Do"
     puts "4) Delete To-Do"
+    puts "Enter any other key to exit"
     print "Choice > "
     input = gets.chomp.to_i
   end
 
   def create_todo
-    puts "What would you like to name your To-do?"
+    puts "What would you like to name your To-Do?"
     input = gets.chomp
     @todo = Todo.find_or_create_by(name: "#{input}")
     @todolist.push(@todo)
@@ -37,15 +62,15 @@ class View
     print "Please enter its corresponding number > "
     input = gets.chomp.to_i
     @todo = Todo.find(input)
-    puts "You chose the To-Do:"
+    puts "You chose the To-Do: "
     old_name = @todo.name
-    puts "#{old_name}\n"
+    print "#{old_name}\n"
     puts "What would you like to re-name this To-Do to? "
     print "New name > "
     new_name = gets.chomp
     @todo.name = new_name
     @todo.save!
-    puts "You renamed the To-Do - #{old_name} - to #{@todo.name}"
+    puts "You renamed the To-Do - #{old_name} - to - #{@todo.name}"
   end
 
   def complete_todo
@@ -53,9 +78,9 @@ class View
     print "Please enter its corresponding number > "
     input = gets.chomp.to_i
     @todo = Todo.find(input)
-    puts "You chose the To-Do:"
+    puts "You chose the To-Do: "
     todo_name = @todo.name
-    puts "#{todo_name}\n"
+    print "#{todo_name}\n"
     puts "Is this To-Do complete? "
     print "Please enter y/n > "
     complete = gets.chomp.downcase
@@ -73,9 +98,9 @@ class View
     print "Please enter its corresponding number > "
     input = gets.chomp.to_i
     @todo = Todo.find(input)
-    puts "You chose the To-Do:"
+    puts "You chose the To-Do: "
     todo_name = @todo.name
-    puts "#{todo_name}\n"
+    print "#{todo_name}\n"
     puts "Are you sure you want to delete the To-Do: #{todo_name}? "
     print "Please enter y/n > "
     complete = gets.chomp.downcase
